2d4846c33afdfe38f62fb0a98a0ffc09949c1e38
zdebs <zdebski1@gmail.com>
2025-04-02 21:27:04 -0400

    adding CORS so frontend can use the APIs locally

--- src/server/app.ts
+++ src/server/app.ts
@@ -2,8 +2,13 @@ import Fastify from 'fastify';
 import availabilityRoutes from '../availability/route';
 import teamRoutes from '../team/team.route';
 import teamMemberRoutes from '../team/teamMember.route';
+import cors from '@fastify/cors';
 
 const fastify = Fastify({ logger: true });
+
+fastify.register(cors, {
+    origin: true,
+  });
 
 fastify.register(availabilityRoutes);
 fastify.register(teamRoutes);

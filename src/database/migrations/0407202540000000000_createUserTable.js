e5384f8fa587a62f23568bd5d75e3017f0d6c8dc
zdebs <zdebski1@gmail.com>
2025-04-07 13:24:11 -0400

    updating knex migration script to create user table

--- src/database/migrations/0407202540000000000_createUserTable.js
+++ src/database/migrations/0407202540000000000_createUserTable.js
@@ -3,11 +3,7 @@ exports.up = function(knex) {
       table.increments('userId').primary();
       table.string('userName');
       table.string('password');
-      table.string('firstName');
-      table.string('lastName');
-      table.date('birthdate');
-      table.string('steamAccountId');         
-      table.boolean('isActive');
+      table.string('email');
       table.timestamp('createdDateTime').defaultTo(knex.fn.now());
       table.string('createdBy');
       table.timestamp('updatedDateTime');

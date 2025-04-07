c6529493803d7dad3c09cafad866867935607036
zdebs <zdebski1@gmail.com>
2025-04-07 15:52:20 -0400

    removing unneeded logic

--- src/team/teamMember.repository.ts
+++ src/team/teamMember.repository.ts
@@ -1,31 +1,5 @@
 import { User, TeamMember } from "../models";
 
-export async function getAllTeamMembers() {
-  try {
-    const teamMembers = await TeamMember.findAll({
-      include: [
-        {
-          model: User,
-          as: 'user',
-          attributes: [
-            'firstName',
-            'lastName'
-          ],
-        },
-      ],
-      attributes: [
-        'teamMemberId',
-        'isActive',
-        'teamFk',
-      ],
-    });
-
-    return teamMembers.map(teamMember => teamMember.get({ plain: true }));
-  } catch (error) {
-    console.error('Error fetching Team Members:', error);
-  }
-}
-
 export async function getTeamMembersByTeamId(teamId: string) {
   try {
     const teamMembers = await TeamMember.findAll({
--- src/team/teamMember.route.ts
+++ src/team/teamMember.route.ts
@@ -4,18 +4,3 @@ import { getAllTeamMembersHandler } from './teamMember.controller';
 export default async function (fastify: FastifyInstance) {
     fastify.get('/teams/:teamId/members', getAllTeamMembersHandler);
 }
-
-
-
-
-// export default async function (fastify: FastifyInstance) {
-//     fastify.get('/teamMembers', async (request, reply) => {
-//         try {
-//             const teamMembers = await getAllTeamMembers();
-//             reply.send(teamMembers);
-//         } catch (error) {
-//             fastify.log.error(error);
-//             reply.status(500).send({ error: `Failed to fetch team members`  });
-//         }
-//     });
-// }

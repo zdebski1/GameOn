f9a0f04da5b09551ff19ec4ca0a3e533fef6543a
zdebs <zdebski1@gmail.com>
2025-04-09 23:31:07 -0400

    updating error message return for team

--- src/team/team.controller.ts
+++ src/team/team.controller.ts
@@ -3,6 +3,7 @@ import { TeamDto } from './team.dto';
 import { createTeamService, getAllTeamService } from './team.service';
 
 
+
 export async function createTeamHandler (
   request: FastifyRequest<{ Body: TeamDto }>,
   reply: FastifyReply
@@ -12,9 +13,13 @@ export async function createTeamHandler (
     const newTeam = await createTeamService(teamDto);
     return reply.code(201).send(newTeam);
   } catch (error) {
-    console.error('Error creating team: ', error);  
-    return reply.code(500).send({ message: 'Internal Server Error' });
+    console.error('Error creating team: ', error); 
+    
+    if (error instanceof Error && (error as any).statusCode === 409) {
+      return reply.code(409).send({ message: error.message });
   }
+}
+    return reply.code(500).send({ message: 'Internal Server Error' });
 }
 
 

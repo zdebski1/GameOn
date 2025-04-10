c1fbf4d4c0618131b45da60ba6a4e7dba77a3554
zdebs <zdebski1@gmail.com>
2025-04-09 23:26:01 -0400

    fixing user email to lowercase bug

--- src/user/user.repository.ts
+++ src/user/user.repository.ts
@@ -1,20 +1,28 @@
 import User from './user.model';
 import { IUserModel } from './user.interface';
+import { Op, Sequelize } from 'sequelize';
 
 export async function findUserByEmail(email: string) {
   return User.findOne({
-    where: 
-      { email: email.toLowerCase()},
+    where: {
+      [Op.and]: [
+        Sequelize.where(Sequelize.fn('LOWER', Sequelize.col('email')), email.toLowerCase())
+      ],
+    },
   });
 }
 
 export async function findUserByUserName(userName: string) {
   return User.findOne({
-    where: 
-      [{ userName: userName.toLowerCase() } , { isActive: true }],
+    where: {
+      [Op.and]: [
+        Sequelize.where(Sequelize.fn('LOWER', Sequelize.col('userName')), userName.toLowerCase()),
+        { isActive: true },
+      ],
+    },
   });
 }
 
 export async function createUser(userModel: Omit<IUserModel, 'userId'>) {
-  return User.create(userModel);
+  return await User.create(userModel);
 }

7eabf01bb4788f0ce23dd8507ee4ecbe3e5aef77
zdebs <zdebski1@gmail.com>
2025-04-10 17:36:01 -0400

    adding jwt return for auth

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,20 +1,30 @@
-import { findUserByUserName } from "../user/user.repository";
+import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
+import { findUserByUserName } from "../user/user.repository";
 import { HttpError } from "../utils/httpError";
 
-
 export async function loginService ({
-    userName, 
-    password 
+  userName,
+  password
 }: {
-    userName: string,
-    password: string
+  userName: string,
+  password: string
 }) {
-    
-    const user = await findUserByUserName(userName);
+  const user = await findUserByUserName(userName);
 
-    if(!user || !(await bcrypt.compare(password, user.password))) {
-        throw new HttpError('Username or password is incorrect', 401);
-}
-return {message: 'Login Successful', userId: user.userId};
-}
+  if (!user || !(await bcrypt.compare(password, user.password))) {
+    throw new HttpError('Username or password is incorrect', 401);
+  }
+
+  const token = jwt.sign(
+    { userId: user.userId, userName: user.userName },
+    process.env.JWT_SECRET!,
+    { expiresIn: '1h' }
+  );
+
+  return {
+    message: 'Login Successful',
+    userId: user.userId,
+    token,
+  };
+};

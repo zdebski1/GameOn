90b7210a62e7214c17fbe9d072881de21993a3fc
zdebs <zdebski1@gmail.com>
2025-04-10 20:41:09 -0400

    modifying migration script

--- src/database/migrations/0410202540000000000_addUserFkConstraintToTeamAndTeamMemberTable.js
+++ src/database/migrations/0410202540000000000_addUserFkConstraintToTeamAndTeamMemberTable.js
@@ -1,15 +1,5 @@
 exports.up = function(knex) {
   return Promise.all([  
-    
-    knex.schema.alterTable('teamMember', function(table) {
-      table
-        .foreign('userFk')
-        .references('userId')
-        .inTable('user')
-        .onDelete('RESTRICT')
-        .onUpdate('CASCADE');
-      }),
-
       knex.schema.alterTable('team', function (table) {
         table
           .foreign('createdBy')
@@ -40,15 +30,26 @@ exports.up = function(knex) {
         .inTable('user')
         .onDelete('RESTRICT')
         .onUpdate('CASCADE');
-      }),
+
+      table
+        .foreign('teamFk')
+        .references('teamId')
+        .inTable('team')
+        .onDelete('RESTRICT')
+        .onUpdate('CASCADE');
+          
+      table
+        .foreign('userFk')
+        .references('userId')
+        .inTable('user')
+        .onDelete('RESTRICT')
+        .onUpdate('CASCADE');          
+      }),     
     ]);
   };
   
   exports.down = function(knex) {
     return Promise.all([
-      knex.schema.alterTable('teamMember', function(table) {
-      table.dropForeign('userFk');
-    }),
       knex.schema.alterTable('team', function (table) {
         table.dropForeign('createdBy');
         table.dropForeign('updatedBy');
@@ -56,6 +57,8 @@ exports.up = function(knex) {
       knex.schema.alterTable('teamMember', function (table) {
         table.dropForeign('createdBy');
         table.dropForeign('updatedBy');
+        table.dropForeign('userFk');
+        table.dropForeign('teamFk');        
     }),
   ]);
 };
